2a5bb152b0724fee859ff8113f3263fcd436e225
Code with Sushant <sushantnirphal@gmail.com>
2023-05-03 21:14:49 +0530

    doc manuals

--- client/src/partials/code/Manual.tsx
+++ client/src/partials/code/Manual.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC, useEffect} from "react";
 import aims from "../aims";
 import FileViewer from "react-file-viewer";
 
@@ -15,6 +15,7 @@ export interface ItemType {
   id: number;
 }
 const Manual: FC<{manual: ItemType}> = ({manual: item}) => {
+  useEffect(() => {}, [item.id, item.aim]);
   return (
     <main className="flex-1 w-full h-full relative">
       <h4 className="p-6 py-4 z-10git checkout -b <branch_name> flex-1 backdrop-filter backdrop-blur-lg bg-slate-900/40 absolute top-0 w-full text-slate-200 text-2xl font-medium border-b z-20 ">
@@ -22,6 +23,7 @@ const Manual: FC<{manual: ItemType}> = ({manual: item}) => {
       </h4>
       <main className="w-full pt-24 overflow-auto max-h-full">
         <FileViewer
+          key={item.manual.url}
           fileType={item?.manual?.type}
           filePath={item?.manual?.url}
         />
