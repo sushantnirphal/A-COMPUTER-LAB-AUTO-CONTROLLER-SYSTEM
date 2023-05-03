ee9ed0815fb99975a001b6fd6b9c5a653bec87b7
Priyanka <malikarpriyanka@gmail.com>
2023-05-03 17:50:16 +0530

    ...

---
+++ client/src/pages/SubmitManual.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "@/partials/Header";
+
+
+
+const SubmitManual = () => {
+ 
+  return (
+    <div className="h-screen overflow-y-auto gr-bg">
+      <Header/>
+      <div className="p-12 md:p-32 h-full  overflow-y-auto text-slate-100"> 
+      <h6 className="text-2xl md:text-2xl lg:text-2xl py-4 text-gr bg-clip-text text-transparent font-semibold">
+        Manual Submission
+      </h6>
+    </div>
+    </div>
+        
+    );
+};
+
+export default SubmitManual;
